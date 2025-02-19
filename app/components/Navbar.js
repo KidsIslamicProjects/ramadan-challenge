f2c9bd8fe5b8565d20c29d133520dbe592916f25
sscout-challenges <hkhodor29@gmail.com>
2025-02-19 13:33:44 +0200

    Remove Confilcts

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
@@ -41,47 +39,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-=======
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import React, { useState, useEffect } from "react";
-import Logo from "../data/images/logo.svg";
-import Hand from "../data/images/Hand.svg";
-import { FaWhatsapp } from "react-icons/fa";
-
-const Navbar = () => {
-  const [userId, setUserId] = useState(null);
-
-  useEffect(() => {
-    setUserId(localStorage.getItem("userId"));
-  }, []);
-  return (
-    <nav className="bg-[#F4F4F3] p-4 flex justify-between items-center">
-      {userId ? (
-        <div className="flex gap-2">
-          {" "}
-          <span className="text-main semi">مرحباً بك</span>
-          <Image alt="hand" src={Hand} className="w-5 mb-1 wave-animation" />
-        </div>
-      ) : (
-        <Link href="/login">
-          <button className="bg-main text-white px-2 py-1 rounded-sm regular">
-            تسجيل دخول
-          </button>
-        </Link>
-      )}{" "}
-      <Image src={Logo} alt="Logo" width={80} height={40} />
-      <Link
-        href="https://wa.me/96171708103"
-        className="flex gap-2 items-center"
-      >
-        <FaWhatsapp />
-        <span className="text-main semi">مجموعة الواتساب</span>
-      </Link>
-    </nav>
-  );
-};
-
-export default Navbar;
->>>>>>> e8a0b93e4ade8906b6713cf98a567d34cb64f685
--- app/components/Notification.js
+++ app/components/Notification.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
@@ -34,40 +33,3 @@ const Notification = ({ message, type }) => {
 };
 
 export default Notification;
-=======
-"use client";
-import React, { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-
-const Notification = ({ message, type }) => {
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    if (message) {
-      setShow(true);
-      const timer = setTimeout(() => setShow(false), 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [message]);
-
-  return (
-    <AnimatePresence>
-      {show && (
-        <motion.div
-          initial={{ x: "100%", opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: "100%", opacity: 0 }}
-          transition={{ duration: 0.5 }}
-          className={`fixed top-5 right-5 z-50 p-4 rounded shadow-lg text-white ${
-            type === "success" ? "bg-green-700" : "bg-red-700"
-          }`}
-        >
-          {message}
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
-};
-
-export default Notification;
->>>>>>> e8a0b93e4ade8906b6713cf98a567d34cb64f685
